d93705b7e103f813d737ec1c8527e75dd2091188
takamitsu-iida <takamitsu.iida@gmail.com>
2017-05-08 15:14:22 +0900

    20170508 initial commit

---
+++ scripts/k5-delete-router.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+DELETE /v2.0/routers/{router_id}
+Delete router
+論理ルータを削除する
+外部ゲートウェイインターフェイスがある場合はそれを削除する
+"""
+
+"""
+実行例（成功した場合は特にデータは戻りません）
+
+bash-4.4$ ./k5-delete-router.py ad3ddc47-6303-48e8-87ad-cb0333c93112
+status_code: 204
+
+実行例（失敗した場合はエラーメッセージが戻ります）
+
+bash-4.4$ ./k5-delete-router.py ad3ddc47-6303-48e8-87ad-cb0333c93112
+{
+  "Content-Type": "application/json",
+  "data": {
+    "NeutronError": "Router ad3ddc47-6303-48e8-87ad-cb0333c93112 could not be found",
+    "request_id": "a1370cc6-b26f-477f-a7c9-a1f6885141f7"
+  },
+  "status_code": 404
+}
+"""
+
+import json
+import logging
+import os
+import sys
+
+# 通常はWARN
+# 多めに情報を見たい場合はINFO
+logging.basicConfig(level=logging.WARN)
+
+def here(path=''):
+  """相対パスを絶対パスに変換して返却します"""
+  return os.path.abspath(os.path.join(os.path.dirname(__file__), path))
+
+# libフォルダにおいたpythonスクリプトを読みこませるための処理
+sys.path.append(here("../lib"))
+sys.path.append(here("../lib/site-packages"))
+
+try:
+  from k5c import k5c
+except ImportError as e:
+  logging.error("k5cモジュールのインポートに失敗しました")
+  logging.error(e)
+  exit(1)
+
+try:
+  from k5c import k5config  # need info in k5config.py
+except ImportError:
+  logging.error("k5configモジュールの読み込みに失敗しました。")
+  exit(1)
+
+
+#
+# メイン
+#
+def main(router_id=''):
+  """メイン関数"""
+  # 接続先
+  url = k5config.EP_NETWORK + "/v2.0/routers/" + router_id
+
+  # Clientクラスをインスタンス化
+  c = k5c.Client()
+
+  # DELETEメソッドで削除して、結果のオブジェクトを得る
+  r = c.delete(url=url)
+
+  # ステータスコードは'status_code'キーに格納
+  status_code = r.get('status_code', -1)
+
+  # ステータスコードが異常な場合
+  if status_code < 0 or status_code >= 400:
+    print(json.dumps(r, indent=2))
+    return r
+
+  # 結果表示
+  print("status_code: {0}".format(r.get('status_code', "")))
+  print(r.get('data', ""))
+
+  # 結果を返す
+  return r
+
+
+if __name__ == '__main__':
+  if len(sys.argv) == 1:
+    print("Usage: {0} {1}".format(sys.argv[0], "router_id"))
+    exit(1)
+
+  main(router_id=sys.argv[1])
---
+++ scripts/k5-list-routers.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+GET /v2.0/routers
+List routers
+テナント内でアクセス可能なルータのリストを表示する。
+"""
+
+"""
+実行例
+
+"""
+
+import json
+import logging
+import os
+import sys
+
+# 通常はWARN
+# 多めに情報を見たい場合はINFO
+logging.basicConfig(level=logging.WARN)
+
+def here(path=''):
+  """相対パスを絶対パスに変換して返却します"""
+  return os.path.abspath(os.path.join(os.path.dirname(__file__), path))
+
+# libフォルダにおいたpythonスクリプトを読みこませるための処理
+sys.path.append(here("../lib"))
+sys.path.append(here("../lib/site-packages"))
+
+try:
+  from k5c import k5c
+except ImportError as e:
+  logging.error("k5cモジュールのインポートに失敗しました")
+  logging.error(e)
+  exit(1)
+
+try:
+  from k5c import k5config  # need info in k5config.py
+except ImportError:
+  logging.error("k5configモジュールの読み込みに失敗しました。")
+  exit(1)
+
+try:
+  from tabulate import tabulate
+except ImportError as e:
+  logging.error("tabulateモジュールのインポートに失敗しました")
+  exit(1)
+
+#
+# メイン
+#
+def main(dump=False):
+  """メイン関数"""
+  # 接続先
+  url = k5config.EP_NETWORK + "/v2.0/routers"
+
+  # Clientクラスをインスタンス化
+  c = k5c.Client()
+
+  # GETメソッドで取得して、結果のオブジェクトを得る
+  r = c.get(url=url)
+
+  # 中身を確認
+  if dump:
+    print(json.dumps(r, indent=2))
+    return r
+
+  # ステータスコードは'status_code'キーに格納
+  status_code = r.get('status_code', -1)
+
+  # ステータスコードが異常な場合
+  if status_code < 0 or status_code >= 400:
+    print(json.dumps(r, indent=2))
+    return r
+
+  # データは'data'キーに格納
+  data = r.get('data', None)
+  if not data:
+    logging.error("no data found")
+    return r
+
+  # ルータ一覧はデータオブジェクトの中の'routers'キーに配列として入っている
+  #"data": {
+  #  "routers": [
+  #    {
+  #      "routes": [],
+  #      "tenant_id": "a5001a8b9c4a4712985c11377bd6d4fe",
+  #      "status": "ACTIVE",
+  #      "name": "iida-test-router-1",
+  #      "id": "5359cce0-cd5c-48e8-891a-659c5ae62f65",
+  #      "external_gateway_info": null,
+  #      "availability_zone": "jp-east-1a",
+  #      "admin_state_up": true
+  #    },
+  routers_list = []
+  for item in data.get('routers', []):
+    routers_list.append([item.get('id', ''), item.get('name', ''), item.get('tenant_id', ''), item.get('availability_zone', ''), item.get('status', '')])
+
+  # 一覧を表示
+  print("GET /v2.0/routers")
+  print(tabulate(routers_list, headers=['id', 'name', 'tenant_id', 'az', 'status'], tablefmt='rst'))
+
+  # 結果を返す
+  return r
+
+
+if __name__ == '__main__':
+  main(dump=False)
