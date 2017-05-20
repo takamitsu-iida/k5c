2d7b61f38095a8fcb2182e2b8a1d722b1b5e6e8b
takamitsu-iida <takamitsu.iida@gmail.com>
2017-05-20 20:09:55 +0900

    20170520 initial commit

---
+++ bin/k5-delete-fw-policy.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+DELETE /v2.0/fw/firewall_policies/{firewall_policy_id}
+Delete firewall policy
+ファイアーウォールポリシーを削除する
+"""
+
+"""
+実行例（成功した場合は特にデータは戻りません）
+
+bash-4.4$ ./bin/k5-delete-fw-policy.py 84417ab6-53ea-4595-9d92-7a9d9a552e12
+status_code: 204
+"""
+
+import json
+import logging
+import os
+import sys
+
+def here(path=''):
+  """相対パスを絶対パスに変換して返却します"""
+  return os.path.abspath(os.path.join(os.path.dirname(__file__), path))
+
+# libフォルダにおいたpythonスクリプトを読みこませるための処理
+if not here("../lib") in sys.path:
+  sys.path.append(here("../lib"))
+
+if not here("../lib/site-packages") in sys.path:
+  sys.path.append(here("../lib/site-packages"))
+
+try:
+  from k5c import k5c
+except ImportError as e:
+  logging.exception("k5cモジュールのインポートに失敗しました: %s", e)
+  sys.exit(1)
+
+
+#
+# APIにアクセスする
+#
+def access_api(policy_id=""):
+  """REST APIにアクセスします"""
+
+  # 接続先
+  url = k5c.EP_NETWORK + "/v2.0/fw/firewall_policies/" + policy_id
+
+  # Clientクラスをインスタンス化
+  c = k5c.Client()
+
+  # DELETEメソッドで削除して、結果のオブジェクトを得る
+  r = c.delete(url=url)
+
+  return r
+
+
+#
+# 結果を表示する
+#
+def print_result(result, dump=False):
+  """結果を表示します"""
+
+  # 中身を確認
+  if dump:
+    print(json.dumps(result, indent=2))
+    return
+
+  # ステータスコードは'status_code'キーに格納
+  status_code = result.get('status_code', -1)
+
+  # ステータスコードが異常な場合
+  if status_code < 0 or status_code >= 400:
+    print(json.dumps(result, indent=2))
+    return
+
+  # 結果表示
+  print("status_code: {0}".format(result.get('status_code', "")))
+  print(result.get('data', ""))
+
+
+if __name__ == '__main__':
+
+  import argparse
+
+  def main():
+    """メイン関数"""
+    parser = argparse.ArgumentParser(description='Deletes a firewall policy.')
+    parser.add_argument('policy_id', metavar='id', help='The firewall policy id.')
+    parser.add_argument('--dump', action='store_true', default=False, help='Dump json result and exit.')
+    args = parser.parse_args()
+    policy_id = args.policy_id
+    dump = args.dump
+
+    if policy_id == '-':
+      import re
+      regex = re.compile('^([a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}).*', re.I)
+      for line in sys.stdin:
+        match = regex.match(line)
+        if match:
+          uuid = match.group(1)
+          # 実行
+          result = access_api(policy_id=uuid)
+          # 表示
+          print(uuid)
+          print_result(result, dump=dump)
+          print("")
+          sys.stdout.flush()
+      return 0
+
+    # 実行
+    result = access_api(policy_id=policy_id)
+
+    # 表示
+    print_result(result, dump=dump)
+
+    return 0
+
+
+  # 実行
+  sys.exit(main())
