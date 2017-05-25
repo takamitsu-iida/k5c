14395820571f7be24c96eb7628a9f29bf5d06ce4
takamitsu-iida <takamitsu.iida@gmail.com>
2017-05-25 13:46:36 +0900

    20170525 add comments

--- bin/k5-list-networks.py
+++ bin/k5-list-networks.py
@@ -10,23 +10,26 @@ List networks
 """
 実行例
 
-bash-4.4$ ./k5-list-networks.py
+bash-4.4$ ./bin/k5-list-networks.py
 GET /v2.0/networks
-====================================  ===================  ================================  ==========  ========
-id                                    name                 tenant_id                         az          status
-====================================  ===================  ================================  ==========  ========
-375c49fa-a706-4676-b55b-2d3554e5db6a  inf_az2_ext-net01    31ceb599e8ff48aeb66f2fd748988960  jp-east-1b  ACTIVE
-4516097a-84dd-476f-824a-6b2fd3cc6499  inf_az2_ext-net05    31ceb599e8ff48aeb66f2fd748988960  jp-east-1b  ACTIVE
-852e40a7-82a3-4196-8b84-46f55d01ccba  inf_az2_ext-net02    31ceb599e8ff48aeb66f2fd748988960  jp-east-1b  ACTIVE
-abe76a93-87c3-4635-b0f3-40f794165c26  inf_az2_ext-net03    31ceb599e8ff48aeb66f2fd748988960  jp-east-1b  ACTIVE
-bfca06b3-0b23-433f-96af-4f54bf963e5f  inf_az2_ext-net04    31ceb599e8ff48aeb66f2fd748988960  jp-east-1b  ACTIVE
-6d9df982-7a89-462a-8b17-8a8e5befa63e  inf_az1_ext-net03    31ceb599e8ff48aeb66f2fd748988960  jp-east-1a  ACTIVE
-92f386c1-59fe-48ca-8cf9-b95f81920466  inf_az1_ext-net02    31ceb599e8ff48aeb66f2fd748988960  jp-east-1a  ACTIVE
-93a83e0e-424e-4e7d-8299-4bdea906354e  iida-test-network-1  a5001a8b9c4a4712985c11377bd6d4fe  jp-east-1a  ACTIVE
-a4715541-c915-444b-bed6-99aa1e8b15c9  inf_az1_ext-net04    31ceb599e8ff48aeb66f2fd748988960  jp-east-1a  ACTIVE
-af4198a9-b392-493d-80ec-a7c6e5a1c22a  inf_az1_ext-net01    31ceb599e8ff48aeb66f2fd748988960  jp-east-1a  ACTIVE
-cd4057bd-f72e-4244-a7dd-1bcb2775dd67  inf_az1_ext-net05    31ceb599e8ff48aeb66f2fd748988960  jp-east-1a  ACTIVE
-====================================  ===================  ================================  ==========  ========
+====================================  =================  ================================  ===================  ========
+id                                    name               tenant_id                         availability_zone    status
+====================================  =================  ================================  ===================  ========
+8f15da62-c7e5-47ec-8668-ee502f6d00d2  iida-az1-net01     a5001a8b9c4a4712985c11377bd6d4fe  jp-east-1a           ACTIVE
+e3c166c0-7e90-4c6e-857e-87fd985f98ac  iida-az1-net02     a5001a8b9c4a4712985c11377bd6d4fe  jp-east-1a           ACTIVE
+0cbf1e4d-479b-4336-8ba9-eb530fe55adb  iida-az2-net01     a5001a8b9c4a4712985c11377bd6d4fe  jp-east-1b           ACTIVE
+8b004a16-c5a2-4e1d-ab9e-a417fef45ec7  iida-az2-net02     a5001a8b9c4a4712985c11377bd6d4fe  jp-east-1b           ACTIVE
+af4198a9-b392-493d-80ec-a7c6e5a1c22a  inf_az1_ext-net01  31ceb599e8ff48aeb66f2fd748988960  jp-east-1a           ACTIVE
+92f386c1-59fe-48ca-8cf9-b95f81920466  inf_az1_ext-net02  31ceb599e8ff48aeb66f2fd748988960  jp-east-1a           ACTIVE
+6d9df982-7a89-462a-8b17-8a8e5befa63e  inf_az1_ext-net03  31ceb599e8ff48aeb66f2fd748988960  jp-east-1a           ACTIVE
+a4715541-c915-444b-bed6-99aa1e8b15c9  inf_az1_ext-net04  31ceb599e8ff48aeb66f2fd748988960  jp-east-1a           ACTIVE
+cd4057bd-f72e-4244-a7dd-1bcb2775dd67  inf_az1_ext-net05  31ceb599e8ff48aeb66f2fd748988960  jp-east-1a           ACTIVE
+375c49fa-a706-4676-b55b-2d3554e5db6a  inf_az2_ext-net01  31ceb599e8ff48aeb66f2fd748988960  jp-east-1b           ACTIVE
+852e40a7-82a3-4196-8b84-46f55d01ccba  inf_az2_ext-net02  31ceb599e8ff48aeb66f2fd748988960  jp-east-1b           ACTIVE
+abe76a93-87c3-4635-b0f3-40f794165c26  inf_az2_ext-net03  31ceb599e8ff48aeb66f2fd748988960  jp-east-1b           ACTIVE
+bfca06b3-0b23-433f-96af-4f54bf963e5f  inf_az2_ext-net04  31ceb599e8ff48aeb66f2fd748988960  jp-east-1b           ACTIVE
+4516097a-84dd-476f-824a-6b2fd3cc6499  inf_az2_ext-net05  31ceb599e8ff48aeb66f2fd748988960  jp-east-1b           ACTIVE
+====================================  =================  ================================  ===================  ========
 """
 
 import json
@@ -79,7 +82,7 @@ def access_api():
 #
 # 結果を表示する
 #
-def print_result(result):
+def print_result(result, az=None):
   """結果を表示します"""
 
   # ステータスコードは'status_code'キーに格納
@@ -121,16 +124,16 @@ def print_result(result):
 
   disp_list = []
   for item in data.get('networks', []):
-    row = []
-    for key in disp_keys:
-      row.append(item.get(key, ''))
-    disp_list.append(row)
+    if az is None or item.get('availability_zone', "") == az:
+      row = []
+      for key in disp_keys:
+        row.append(item.get(key, ''))
+      disp_list.append(row)
 
   # sorted()を使ってnameをもとにソートする
   # nameは配列の2番めの要素なのでインデックスは1
   disp_list = sorted(disp_list, key=lambda x: x[1])
 
-  # 一覧を表示
   print("GET /v2.0/networks")
   print(tabulate(disp_list, headers=disp_keys, tablefmt='rst'))
 
@@ -142,8 +145,10 @@ if __name__ == '__main__':
   def main():
     """メイン関数"""
     parser = argparse.ArgumentParser(description='List networks')
+    parser.add_argument('--az', nargs='?', default=None, help='The Availability Zone name to display.')
     parser.add_argument('--dump', action='store_true', default=False, help='Dump json result and exit.')
     args = parser.parse_args()
+    az = args.az
     dump = args.dump
 
     # 実行
@@ -155,7 +160,7 @@ if __name__ == '__main__':
       return 0
 
     # 表示
-    print_result(result)
+    print_result(result, az=az)
 
     return 0
 
